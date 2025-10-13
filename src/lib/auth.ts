c681d172fbff699524d32622450f447adf6d570e
Frank Behrens <fbehrens@gmail.com>
2025-10-13 20:11:23 +0200

    configure github

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,18 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from './server/db';
-
+import { sveltekitCookies } from 'better-auth/svelte-kit';
+import { getRequestEvent } from '$app/server';
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: 'sqlite' // 'pg', "mysql"
-	})
+	}),
+	socialProviders: {
+		github: {
+			clientId: GITHUB_CLIENT_ID,
+			clientSecret: GITHUB_CLIENT_SECRET
+		}
+	},
+	plugins: [sveltekitCookies(getRequestEvent)] // make sure this is the last plugin in the array
 });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { authClient } from '$lib/auth-client';
+	const session = authClient.useSession();
+</script>
+
+<div>
+	{#if $session.data}
+		<div>
+			<p>
+				{$session.data.user.name}
+			</p>
+			<button
+				onclick={async () => {
+					await authClient.signOut();
+				}}
+			>
+				Sign Out
+			</button>
+		</div>
+	{:else}
+		<button
+			onclick={async () => {
+				alert('bla');
+				await authClient.signIn.social({
+					provider: 'github'
+				});
+			}}
+		>
+			Continue with GitHub
+		</button>
+	{/if}
+</div>
